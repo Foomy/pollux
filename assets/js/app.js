caa45b611af1d7c373ef923e90844be34cdc89f6
Sascha Schneider <sir.foomy@googlemail.com>
2020-02-24 01:01:13 +0100

    [PML #6] creates home layout and home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
